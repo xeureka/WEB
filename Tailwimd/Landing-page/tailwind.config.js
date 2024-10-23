fbc7f75e095e3a99ab0ccab6851c763dd8550749
xeureka <teklemariameureka@gmail.com>
2024-10-23 16:30:23 +0300

    starter of admin dashboard

--- Tailwimd/Landing-page/tailwind.config.js
+++ Tailwimd/Landing-page/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        steelblue: '#4682B4',
+      }
+    },
   },
   plugins: [],
 }

3b4f81847ace03fb2253b2cdd214690bc7f51a60
xeureka <teklemariameureka@gmail.com>
2024-11-05 16:37:08 +0300

    tailwind setup with the project

--- ODIN-PROJECTS/React/CV-APP/src/App.jsx
+++ ODIN-PROJECTS/React/CV-APP/src/App.jsx
@@ -2,25 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [name,setName] = useState(' ')
-
-  // function changeName(){
-  //   let fName = e.target.value;
-
-  //   setName(fName)
-  // }
-
-
   return (
     <div>
-      <h1>This is CV Builder App</h1>
-      <input type="text" onChange={(e) =>{
-        setName(e.target.value)
-        
-      }} value={name.name} placeholder='Eureka'/>
-
-      <p>{name}</p>
-      
+      <p>CV BUILDER APP</p>
     </div>
   )
 }

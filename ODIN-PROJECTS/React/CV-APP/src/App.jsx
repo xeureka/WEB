acd5f6483f31b288bf3d28a8ea70bf528f4dc2a3
xeureka <teklemariameureka@gmail.com>
2024-11-17 02:19:22 +0300

    to do list with react starting

--- ODIN-PROJECTS/React/CV-APP/src/App.jsx
+++ ODIN-PROJECTS/React/CV-APP/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
+import ToDo from './Components/ToDo'
 
 const App = () => {
   return (
     <div>
-      <p>CV BUILDER APP</p>
+      <ToDo />
     </div>
   )
 }

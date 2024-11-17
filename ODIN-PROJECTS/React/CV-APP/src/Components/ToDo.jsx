37c5186a82a1a06879778e0b95331f19dd74b677
xeureka <teklemariameureka@gmail.com>
2024-11-17 23:19:40 +0300

    all logic and tasks of the page are completed

--- ODIN-PROJECTS/React/CV-APP/src/Components/ToDo.jsx
+++ ODIN-PROJECTS/React/CV-APP/src/Components/ToDo.jsx
@@ -1,8 +1,8 @@
 import React,{useState} from 'react'
 
 const ToDo = () => {
-    const [tasks,setTasks] = useState(["Eat Breakfast","Take a shower"]);
-    const [newTask,setNewTask] = useState([]);
+    const [tasks,setTask] = useState(['Eat BreakFast','Take a Shower']);
+    const [newTask,setNewTask] = useState("")
 
     function handleInputChange(event){
         setNewTask(event.target.value)
@@ -10,47 +10,69 @@ const ToDo = () => {
     }
 
     function addTask(){
-        if (newTask){
-            setTasks(t => [...t,newTask]);
-            setTasks("");
+        if(newTask.trim() !== ""){
+            setTask([...tasks,newTask])
+            setNewTask("")
         }
     }
 
     function deleteTask(index){
+        const updatedTask = tasks.filter((_,i) => i !== index);
+        setTask(updatedTask)
 
     }
 
     function moveTaskUp(index){
+        if (index > 0){
+            const updatedTask =  [...tasks];
+            [updatedTask[index],updatedTask[index-1]] = [updatedTask[index-1],updatedTask[index]]
+            setTask(updatedTask);
+        }
 
     }
 
     function moveTaskDown(index){
+        if (index < tasks.length - 1){
+            const updatedTask = [...tasks];
+
+            [updatedTask[index] ,updatedTask[index+1]] =
+            [updatedTask[index+1],updatedTask[index]]
+
+            setTask(updatedTask);
+
+        }
 
     }
 
   return (
     <div>
-      <h1>To-Do-List</h1>
+      <h1>TO Do APPLICTION</h1>
 
       <div>
-        <input type="text" 
-                placeholder='Enter a task ...'
-                value={newTask}
-                onChange={handleInputChange}
-                        />
-        <button onClick={addTask}>Add</button>
+        <input 
+            type="text" 
+            placeholder='Enter Your Task Here....'
+            value={newTask}
+            onChange={handleInputChange}
+            />
+
+            <button onClick={addTask}>Add</button>
       </div>
 
       <ol>
-                {tasks.map((task, index) => (
-                    <li key={index}>
-                        <span>{task}</span>
-                        <button onClick={() => deleteTask(index)}>Delete</button>
-                        <button onClick={() => moveTaskUp(index)}>Up</button>
-                        <button onClick={() => moveTaskDown(index)}>Down</button>
-                    </li>
+
+      <ol>
+        {tasks.map((task, index) => (
+                <li key={index}>
+                    <span>{task}</span>
+                    <button onClick={() => deleteTask(index)}>Delete</button>
+                    <button onClick={() => moveTaskUp(index)}>Up</button>
+                    <button onClick={() => moveTaskDown(index)}>Down</button>
+                </li>
                 ))}
+            </ol>
       </ol>
+
 
     </div>
   )

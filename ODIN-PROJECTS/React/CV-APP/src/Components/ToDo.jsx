3825100d89221ca3175249c3a4f74f347e77c7e3
xeureka <teklemariameureka@gmail.com>
2024-11-17 23:55:25 +0300

    to do app completed

--- ODIN-PROJECTS/React/CV-APP/src/Components/ToDo.jsx
+++ ODIN-PROJECTS/React/CV-APP/src/Components/ToDo.jsx
@@ -44,30 +44,35 @@ const ToDo = () => {
 
     }
 
-  return (
-    <div>
-      <h1>TO Do APPLICTION</h1>
+  return ( 
+    <div className='bg-indigo-400 text-white w-full min-h-screen flex items-center flex-col font-mono '>
+      <h1 className='text-4xl font-bold my-4'>TO DO APP</h1>
 
       <div>
         <input 
             type="text" 
-            placeholder='Enter Your Task Here....'
+            placeholder='Add Task ....'
             value={newTask}
             onChange={handleInputChange}
+            className='px-2 py-4  text-black rounded'
             />
 
-            <button onClick={addTask}>Add</button>
+            <button
+                onClick={addTask}
+                className='mx-12 font-bold text-xl bg-violet-500 px-2 py-3 w-40 rounded hover:bg-violet-400 active:bg-violet-600 my-12'>
+                
+                    Add</button>
       </div>
 
       <ol>
 
       <ol>
         {tasks.map((task, index) => (
-                <li key={index}>
-                    <span>{task}</span>
-                    <button onClick={() => deleteTask(index)}>Delete</button>
-                    <button onClick={() => moveTaskUp(index)}>Up</button>
-                    <button onClick={() => moveTaskDown(index)}>Down</button>
+                <li key={index} className='w-200 p-4 bg-red-500 rounded m-5'>
+                    <span className='font-serif text-black mx-16 text-xl font-bold'>{task}</span>
+                    <button onClick={() => deleteTask(index)} className='bg-slate-500 mx-12 px-4 py-2 font-bold text-lg font-sans rounded hover:bg-slate-700 active:bg-slate-400'>Delete</button>
+                    <button onClick={() => moveTaskUp(index)} className='bg-slate-500 mx-12 px-4 py-2 font-bold text-lg font-sans rounded hover:bg-slate-700 active:bg-slate-400'>Up</button>
+                    <button onClick={() => moveTaskDown(index)} className='bg-slate-500 mx-12 px-4 py-2 font-bold text-lg font-sans rounded hover:bg-slate-700 active:bg-slate-400'>Down</button>
                 </li>
                 ))}
             </ol>
